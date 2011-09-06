4f4606f57828ef9c4f54c54f6657a561c3587cd4
Chris Lamb <chris@playfire.com>
2011-09-06 11:39:36 +0100

    Need to set a default here too.
    Signed-off-by: Chris Lamb <chris@playfire.com>

--- dynamic_subdomains/decorators.py
+++ dynamic_subdomains/decorators.py
@@ -5,7 +5,7 @@ from .middleware import _thread_local
 def disable_subdomain_middleware(fn):
     @functools.wraps(fn)
     def wrapped(*args, **kwargs):
-        prev = _thread_local.enabled
+        prev = getattr(_thread_local, 'enabled', True)
 
         try:
             _thread_local.enabled = False
